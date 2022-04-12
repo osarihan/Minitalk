c051c0274cb31182e2e3a7db5facdb558e1e7ae9
Oguz Sarihan <osarihan@k2m18s04.42kocaeli.com.tr>
2022-04-12 11:45:03 +0300

    server

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -6,22 +6,22 @@
 /*   By: osarihan <osarihan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 17:26:43 by osarihan          #+#    #+#             */
-/*   Updated: 2022/04/11 17:41:00 by osarihan         ###   ########.fr       */
+/*   Updated: 2022/04/12 11:38:59 by osarihan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void ft_send(int pid, char c)
+void	ft_send(int pid, char c)
 {
-	int i;
-	int bit;
+	int	i;
+	int	bit;
 
 	i = 7;
-	while(i >= 0)
+	while (i >= 0)
 	{
 		bit = (c >> i) & 1;
-		if(bit == 0)
+		if (bit == 0)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
@@ -30,26 +30,27 @@ void ft_send(int pid, char c)
 	}
 }
 
-void message_char(int pid, char *str)
+void	message_char(int pid, char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
 		ft_enviar(pid, str[i]);
 		i++;
 	}	
 }
 
-int	main (int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int pid;
-	if(argc < 3)
+	int	pid;
+
+	if (argc < 3)
 		ft_putstr(PIDERROR);
-	if(argc > 3)
+	if (argc > 3)
 		ft_putstr(ARGERROR);
 	pid = ft_atoi(argv[1]);
 	message_char(pid, argv[2]);
-	return(0);
+	return (0);
 }
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -6,7 +6,69 @@
 /*   By: osarihan <osarihan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 17:26:40 by osarihan          #+#    #+#             */
-/*   Updated: 2022/04/11 17:26:41 by osarihan         ###   ########.fr       */
+/*   Updated: 2022/04/12 11:42:13 by osarihan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+void	binarytodecimal(char *bits)
+{
+	int				base;
+	unsigned char	num;
+	int				i;
+
+	num = 0;
+	i = 0;
+	base = 128;
+	while (bits[i])
+	{
+		num += base * (bits[i] - '0');
+		base /= 2;
+		i++;
+	}
+	write(1, &num, 1);
+}
+
+void	ft_signal(int signal)
+{
+	static int	contador;
+	static char	*bits;
+
+	contador++;
+	if (bits == NULL)
+	{
+		bits = ft_strdup("");
+		contador = 1;
+	}
+	if (signal == SIGUSR1)
+		bits = ft_strjoin(bits, '0');
+	else
+		bits = ft_strjoin(bits, '1');
+	if (contador == 8)
+	{
+		binarytodecimal(bits);
+		free(bits);
+		bits = NULL;
+	}
+}
+
+int	main(void)
+{
+	int		pid;
+	char	*pidstr;
+
+	pid = 0;
+	pid = getpid();
+	ft_putstr("PID: ");
+	pidstr = ft_itoa(pid);
+	ft_putstr(pidstr);
+	write(1, "\n", 1);
+	while (1)
+	{
+		signal(SIGUSR1, ft_signal);
+		signal(SIGUSR1, ft_signal);
+		pause();
+	}
+	return (0);
+}
